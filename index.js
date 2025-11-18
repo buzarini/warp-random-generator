e80cb8b3fb29b9e28d0fbe9c6692f7f99935aa7e
DikozImpact <22dikoz22@gmail.com>
2025-11-18 23:28:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { getWarpConfigLink6 } = require('./Throne');
 const { getWarpConfigLink7 } = require('./Neko');
 const { getWarpConfigLink8 } = require('./Husi');
 const { getWarpConfigLink9 } = require('./Karing');
+const { getWarpConfigLink10 } = require('./WireSock');
 const path = require('path');
 
 const app = express();
@@ -144,6 +145,22 @@ app.get('/warp8', async (req, res) => {
 app.get('/warp9', async (req, res) => {
     try {
         const content = await getWarpConfigLink9();
+        if (content) {
+            res.json({ success: true, content });
+        } else {
+            res.status(500).json({ success: false, message: 'Не удалось сгенерировать конфиг.' });
+        }
+    } catch (error) {
+        console.error('Ошибка при обработке запроса:', error);
+        res.status(500).json({ success: false, message: 'Произошла ошибка на сервере.' });
+    }
+});
+
+app.get('/warp10w', async (req, res) => {
+    try {
+        const dns = req.query.dns || "1.1.1.1, 1.0.0.1, 2606:4700:4700::1111, 2606:4700:4700::1001";
+        const allowedIPs = req.query.allowedIPs || "0.0.0.0/0, ::/0";
+        const content = await getWarpConfigLink10(dns, allowedIPs);
         if (content) {
             res.json({ success: true, content });
         } else {
